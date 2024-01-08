c5efc93c5e46efe835037969d9f6d1de5b55c299
Andrew M. Farrell <armorsmith42@gmail.com>
2024-01-07 19:19:42 -0500

    Insert tab as two spaces

--- lua/options.lua
+++ lua/options.lua
@@ -39,4 +39,10 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Set tab to insert 2 spaces.
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
 -- vim: ts=2 sts=2 sw=2 et
