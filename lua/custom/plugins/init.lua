d1175fb3cd1be3517360924faab208e025b15a24
Andrew M. Farrell <123831+amfarrell@users.noreply.github.com>
2025-02-17 21:59:48 -0500

    add neorgit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,29 +6,39 @@ return {
 
   {
     'nvim-orgmode/orgmode',
-    ft = "org",
+    ft = 'org',
     dependencies = {
       { 'nvim-treesitter/nvim-treesitter', lazy = true },
     },
     event = 'VeryLazy',
     config = function()
-      -- Load treesitter grammar for org
-      require('orgmode').setup_ts_grammar()
-
       -- Setup treesitter
-      require('nvim-treesitter.configs').setup({
+      require('nvim-treesitter.configs').setup {
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = { 'org' },
         },
         ensure_installed = { 'org' },
-      })
+      }
 
       -- Setup orgmode
-      require('orgmode').setup({
+      require('orgmode').setup {
         org_agenda_files = '~/orgfiles/**/*',
         org_default_notes_file = '~/orgfiles/refile.org',
-      })
+      }
     end,
-  }
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      --      "ibhagwan/fzf-lua",              -- optional
+      --      "echasnovski/mini.pick",         -- optional
+    },
+    config = true,
+  },
 }
